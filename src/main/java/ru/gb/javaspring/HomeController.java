77510f4067b5b538d07bee0e39944b6bad2074de
Eugene <popovevgeni81@gmail.com>
2024-01-27 10:31:32 +0800

    lesson4

---
+++ src/main/java/ru/gb/javaspring/HomeController.java
@@ -0,0 +1,12 @@
+package ru.gb.javaspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}
